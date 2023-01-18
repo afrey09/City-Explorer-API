c1eab18710822c6796de9db1da450ac78b66ce0f
Adrienne Frey <AdrienneN.Frey@gmail.com>
2023-01-17 20:24:27 -0800

    began process to link front-end and back-end

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 
+//*** FOR LAB - DON'T FORGET TO REQUIRE YOUR START JSON FILE *** 
+let data = require('./data/weather.json');
+
 
 
 //**** Once express is in we need to use it - per express docs *
@@ -29,11 +32,63 @@ const PORT = process.env.PORT || 3002;
 //*** 2nd argument - callback which will execute when someone hit that point ***
 
 //**** Callback function - 2 parameters: Request, response (req,res) */
-app.get('/', (request, response)=>{
+app.get('/', (request, response) => {
   response.status(200).send('Welcome to my server');
+});
+
+app.get('/hello', (request, response) => {
+  console.log(request.query);
+
+  let firstName = request.query.firstName;
+  let lastName = request.query.lastName;
+
+  response.status(200).send(`Hello ${firstName} ${lastName}! Welcome to my server!`);
 });
 
 
 
+
+
+app.get('/weather', (req, res, next) => {
+  try {
+    let lat = req.query.lat;
+    let lon = req.query.lon;
+    let searchQuery = req.query.searchQuery;
+
+    let dataToGroom = data.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+    
+    let dataToSend = new Forecast(dataToGroom);
+
+    res.status(200).send(dataToSend);
+
+
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+//**** CLASS TO GROOM BULKY DATA */
+
+class Forecast {
+  constructor(forecastObj) {
+    this.date = forecastObj.date;
+    this.description = forecastObj.description;
+  }
+}
+
+
+//**** CATCH ALL ENDPOINT - NEEDS TO BE YOUR LAST DEFINED ENDPOINT **** 
+
+app.get('*', (request, response) => {
+  response.status(404).send('This page does not exist');
+});
+
+//**** ERROR HANDLING - PLUG & PLAY FROM EXPRESS DOCS - SHOULD ALSO LIVE AT BOTTOM OF PAGE ****
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+
 //**** SERVER START **
-app.listen(PORT, ()=>console.log(`We are running on port: ${PORT}`));
+app.listen(PORT, () => console.log(`We are running on port: ${PORT}`));
